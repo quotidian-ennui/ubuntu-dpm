725fb8b88a1b814c6948a3067013f47e6253ff97
Lewin Chan <quotidian-ennui@users.noreply.github.com>
2024-07-13 21:21:36 +0100

    refactor: use sourced functions not explicit scripts (#251)
    - make sdk_install_ sourced functions
    - make init_ a sourced function

---
+++ src/main/scripts/init.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+BASEDIR=$(dirname "$0")
+#shellcheck disable=SC1091
+source "$BASEDIR/common.sh"
+ACTION_LIST=()
+
+source_actions() {
+  for f in "$BASEDIR"/includes/init_*; do
+    #shellcheck disable=SC1090
+    source "$f"
+    name=$(basename "$f")
+    ACTION_LIST+=("${name}")
+  done
+}
+
+source_actions
+for action in "${ACTION_LIST[@]}"; do
+  "${action}"
+done
