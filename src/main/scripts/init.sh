0fe29a7bba389dea92a0ca3f2aa292da22cb4372
Lewin Chan <8480608+quotidian-ennui@users.noreply.github.com>
2025-08-13 16:51:05 +0100

    fix: remove direnv stdlib

--- src/main/scripts/init.sh
+++ src/main/scripts/init.sh
@@ -7,7 +7,7 @@ source "$BASEDIR/common.sh"
 ACTION_LIST=()
 
 source_actions() {
-  for f in "$BASEDIR"/includes/init_*; do
+  find -L "$BASEDIR/includes" -type f -name "init_*" -print0 | while read -d $'\0' -r f; do
     #shellcheck disable=SC1090
     source "$f"
     name=$(basename "$f")
